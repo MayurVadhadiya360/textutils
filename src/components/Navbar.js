d9cf07a423822ab6793046e7ec73f931b016188d
Mayur <mayurvadhadiya5@gmail.com>
2023-12-25 18:23:45 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,7 +17,7 @@ export default function Navbar(props) {
               <Link className="nav-link active" aria-current="page" to="/textutils">Home</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="textutils/about">{props.aboutText}</Link>
+              <Link className="nav-link" to="/textutils/about">{props.aboutText}</Link>
             </li>
             
           </ul>
